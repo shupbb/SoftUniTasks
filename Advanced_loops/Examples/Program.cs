42c8fb142a0a68264b6a89661e5de51940265115
shupbb <shupbb@gmail.com>
2018-05-18 14:54:33 +0300

    settings

---
+++ Advanced_loops/Examples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 1; j <= 2; j++)
+                {
+                    Console.WriteLine("hello");
+                }
+            }
+        }
+    }
+}
+            31:00
